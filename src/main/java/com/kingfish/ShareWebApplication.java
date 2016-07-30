0f0945908cfccf2d5a66a80b8190f3955eb7a6b7
康真乐 <zhenle.kzl@alibaba-inc.com>
2016-07-30 20:38:07 +0800

    spring web project init

---
+++ src/main/java/com/kingfish/ShareWebApplication.java
@@ -0,0 +1,12 @@
+package com.kingfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareWebApplication.class, args);
+	}
+}
